fbdcb505284208977793a9c974ca8545b2f270d6
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2015-03-29 12:41:15 +0530

    Fix: conversion issues

--- gruntfile.js
+++ gruntfile.js
@@ -367,6 +367,10 @@ var convertToUnicodeCLI = function(text, mappingString) {
     text = text.replace(/ੑੀ/g,"ੀੑ")
     text = text.replace(/ੑੇ/g,"ੇੑ")
     text = text.replace(/ਂੀ/g,"ੀਂ")
+    text = text.replace(/ਿੰੵ/g,"ੵਿੰ")
+    text = text.replace(/ੰੵ/g,"ੵੰ")
+    text = text.replace(/ੑੁ/g,"ੁੑ")
+    text = text.replace(/ੑੈ/g,"ੈੑ")
 
     // text = text.split('<ਬਰ>').join('</p><p>');
     // text = text.split('<ਬਰ>').join('</p><p>');
